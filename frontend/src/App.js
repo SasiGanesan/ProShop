7ae1214a6102d8d01615e38a512c4bc9e2e0ca58
SasiGanesan <sasiganesan2001@gmail.com>
2023-11-29 18:58:43 +0530

    Frontend completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@
 import { Container } from 'react-bootstrap';
 import Header from './components/Header'
 import Footer from './components/Footer';
+//import HomeScreen from './screens/HomeScreen';
+import { Outlet } from 'react-router-dom';
 function App() {
   return (
     <>
     <Header/>
     <main className='py-3'>
       <Container>
-        <h1>Welcome To ProShop</h1>
+        <Outlet />
       </Container>
   </main>
   <Footer/>
