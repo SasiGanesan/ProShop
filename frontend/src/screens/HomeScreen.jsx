8231a4ec2244eb99ef4e01ef818a6cb3ce34cb29
SasiGanesan <sasiganesan2001@gmail.com>
2023-12-01 11:53:55 +0530

    frontend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,18 @@
 //import React from 'react'
+import {  useEffect,useState } from 'react'
 import {Row,Col} from 'react-bootstrap'
 import Product from '../components/Product'
 
 const HomeScreen = () => {
+  const [products, setproducts]=useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async() =>{
+      const {data}= await axios.get('/api/products');
+      setproducts(data);
+    };
+    fetchProducts();
+  },[]);
   return (
     <>
       <h1>Latest Products</h1>
