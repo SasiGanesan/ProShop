cd688f60bbb167cd0457f91fd5c952d748dc765d
SasiGanesan <sasiganesan2001@gmail.com>
2023-12-04 20:17:32 +0530

    backend

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,5 +1,5 @@
-const notFound=(req,res,next)=>{
-    const error= new Error(`Not Found - ${req.originalUrl}`);
+const notFound=(req,res,next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
 };
@@ -7,6 +7,18 @@ const notFound=(req,res,next)=>{
 
 
 const errorHandler=(err, req,res,next)=>{
-    let statusCode = res.statusCode ===200?500 :res.statusCode;
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = err.message;
+
+    if(err.name === 'CastError' && err.kind === 'ObjectId'){
+        message = `Resource not found`;
+        statusCode = 404;
+    }
+    res.status(statusCode).json({
+        message,
+        stack: process.env.NODE_ENV ==='production' ? 'pancake': err.stack,
+    });
 };
+
+export {notFound, errorHandler}
 
