abeb24be5a7ee3f02ef2e7e657517f566b41fe29
SasiGanesan <sasiganesan2001@gmail.com>
2023-12-04 17:49:35 +0530

    mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,19 @@
-import express from 'express'
-import dotenv from 'dotenv'
-
-import connectDB from './config/db.js';
-import products from './data/products.js';
-import productRoutes from './routes/productRoutes.js'
-
+import express from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
-connectDB();//connect to MongoDB
-
+import connectDB from './config/db.js';
+//import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 const port=process.env.PORT || 5000;
 
-const app=express()
+connectDB();//connect to MongoDB
+
+const app=express();
 
 app.get('/',(req,res)=>{
     res.send('API is running...');
 });
 
+app.use('/api/products', productRoutes);
 
-app.use('/api/products', productRoutes)
-
-app.listen(port, ()=>console.log(`Server running on port ${port}`))
+app.listen(port, ()=>console.log(`Server running on port ${port}`));
